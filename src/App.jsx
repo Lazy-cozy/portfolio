c786b07403df18fbb37e3326ff0b8418897e334b
Khalid <khalid@freelance.com>
2023-12-03 13:10:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./scss/global.scss";
+
+function App() {
+  return (
+    <main className="home-page-container">
+      <div className="home-Header"></div>
+      <div className="home-body">
+        <div>
+          <h1>Hi,Iam a web</h1>
+          <h1>Web</h1>
+          <h1>Developer</h1>
+        </div>
+        <div>
+          <button>Get in Touch</button>
+        </div>
+      </div>
+      <div className="home-footer"></div>
+    </main>
+  );
+}
+
+export default App;
